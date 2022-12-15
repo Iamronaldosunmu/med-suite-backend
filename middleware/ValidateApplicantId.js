1117c2f4b368ef34341a95d5e6464ad703294f4a
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2022-12-15 21:23:43 +0100

    Added the middleware for validating applicant ids

---
+++ middleware/ValidateApplicantId.js
@@ -0,0 +1,23 @@
+import { isValidObjectId } from "mongoose";
+import Applicant from "../models/applicant.js";
+import { StatusCodes } from "http-status-codes";
+
+export const validateApplicantId = async (req, res, next) => {
+  const { applicantId } = req.params;
+
+  if (!isValidObjectId(applicantId))
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ message: "This applicant ID is not valid" });
+
+  let applicant = await Applicant.findOne({ _id: applicantId });
+  if (!applicant)
+    return res
+      .status(StatusCodes.NOT_FOUND)
+      .json({ message: "This applicant does not exist!" });
+
+  req.applicant = applicant;
+  req.applicantId = applicantId;
+
+  next();
+};
