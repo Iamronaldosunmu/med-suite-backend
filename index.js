5ef6b17d0ae09f554f0b44f4d3369d1c37e46288
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2022-12-18 20:25:49 +0100

    Added the middleware function to app.js

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ import connectToDB from "./startup/db.js";
 import useRoutesFor from "./startup/routes.js";
 import dotenv from "dotenv";
 import config from "./startup/config.js";
+import middleware from "./startup/middleware.js";
 
 dotenv.config();
 config();
 const app = express();
 app.use(express.json());
+middleware(app);
 useRoutesFor(app);
 
 app.listen(process.env.PORT || 4000, () =>
