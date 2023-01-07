36f0e37781cc446119136593c17ad7515fc4a524
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2023-01-07 14:58:27 +0100

    Added controller to approve an applicant

--- controllers/applicant.js
+++ controllers/applicant.js
@@ -2,3 +2,22 @@ export const getApplicantById = async (req, res) => {
   const { applicant, applicantId } = req;
   return res.status(200).json({ applicant });
 };
+
+export const approveApplicant = async (req, res) => {
+  const { applicant, applicantId } = req;
+  const hasAllDocumentsApproved = Object.values(
+    applicant.doumentReviewStatuses._doc
+  )
+    .filter((status) => typeof status == "string")
+    .every((value) => value == "Accepted");
+
+  if (!hasAllDocumentsApproved)
+    return res
+      .status(400)
+      .json({ message: "Not all Documents have been Accepted Yet" }); 4
+  
+  applicant.status = "Approved"
+  await applicant.save();
+
+  return res.status(200).json({message: "Applicant Approved Successfully!"})
+};
