5b6d7842abae851859f4616efc0ab02ab670054a
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2023-01-07 15:05:35 +0100

    Added the route to approve an applicant

--- routes/applicant.js
+++ routes/applicant.js
@@ -1,8 +1,16 @@
 import express from "express";
-import { getApplicantById } from "../controllers/applicant.js";
+import {
+  approveApplicant,
+  getApplicantById,
+} from "../controllers/applicant.js";
 import { validateApplicantId } from "../middleware/ValidateApplicantId.js";
 
 const applicantRouter = express.Router();
 applicantRouter.get("/:applicantId", validateApplicantId, getApplicantById);
+applicantRouter.post(
+  "/approve_applicant/:applicantId",
+  validateApplicantId,
+  approveApplicant
+);
 
 export default applicantRouter;
