66d4dd3ff0882eaf20560065c4f085239765081e
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2023-01-09 13:16:50 +0100

    Send message when applicant is approved

--- controllers/applicant.js
+++ controllers/applicant.js
@@ -1,3 +1,5 @@
+import MessageService from "../services/MessageService.js";
+
 export const getApplicantById = async (req, res) => {
   const { applicant, applicantId } = req;
   return res.status(200).json({ applicant });
@@ -14,10 +16,20 @@ export const approveApplicant = async (req, res) => {
   if (!hasAllDocumentsApproved)
     return res
       .status(400)
-      .json({ message: "Not all Documents have been Accepted Yet" }); 4
-  
-  applicant.status = "Approved"
+      .json({ message: "Not all Documents have been Accepted Yet" });
+
+  applicant.status = "Approved";
+  try {
+    await MessageService.sendMessage(
+      applicantId,
+      "admin",
+      `Your profile has been approved Successfully! \nWe will communicate the date of your online interview to you in the near future.`
+    );
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ message: error.message });
+  }
   await applicant.save();
 
-  return res.status(200).json({message: "Applicant Approved Successfully!"})
+  return res.status(200).json({ message: "Applicant Approved Successfully!" });
 };
