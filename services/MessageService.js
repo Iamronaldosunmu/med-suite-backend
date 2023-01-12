0dd260248dbfaba8431ce0935ccce5b24f7911db
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2023-01-13 00:09:28 +0100

    Created the Message Service Class

--- services/MessageService.js
+++ services/MessageService.js
@@ -1,12 +1,31 @@
-import Chat from "../models/chat";
+import { isValidObjectId } from "mongoose";
+import Chat from "../models/chat.js";
 
 class MessageService {
   static async sendMessage(applicantId, from, content) {
-    const message = { from, content, status: "unread" };
-    const chat = await Chat.findOne({ applicant: applicantId });
+    if (!isValidObjectId(applicantId)) throw new Error("This Id is not valid");
+    if (from !== "admin" && from !== "applicant")
+      throw new Error("The sender is not valid!");
+    let message = { from, content, status: "unread" };
+    let chat = await Chat.findOne({ applicant: applicantId });
     if (!chat)
       throw new Error("Not Found: No Chat with this applicant ID exists");
     chat.messages.push(message);
+    await chat.save();
+  }
+
+  static async markAllMessagesAsRead(applicantId) {
+    if (!isValidObjectId(applicantId)) throw new Error("This Id is not valid");
+
+    let chat = await Chat.findOne({ applicant: applicantId });
+    if (!chat)
+      throw new Error("Not Found: No Chat with this applicant ID exists");
+
+    for (let messageIndex in chat.messages) {
+      if (chat.messages[messageIndex].status == "unread") {
+        chat.messages[messageIndex].status = "read";
+      }
+    }
     await chat.save();
   }
 }
