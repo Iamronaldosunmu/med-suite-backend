a1b5eb0436abd697a6f0853f5849194450cc0a59
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2023-01-13 00:04:04 +0100

    Added the controller for getting all applicants

--- controllers/applicant.js
+++ controllers/applicant.js
@@ -1,8 +1,28 @@
+import Applicant from "../models/applicant.js";
 import MessageService from "../services/MessageService.js";
 
 export const getApplicantById = async (req, res) => {
   const { applicant, applicantId } = req;
   return res.status(200).json({ applicant });
+};
+
+export const getAllApplicants = async (req, res) => {
+  const applicants = await Applicant.find({}).populate("user", "-password");
+  return res.status(200).json({ applicants });
+};
+
+export const getApplicantNumbers = async (req, res) => {
+  const total_applications = await Applicant.find({}).count();
+  const being_reviewed = await Applicant.find({
+    status: "Being Reviewed",
+  }).count();
+  const approved_applications = await Applicant.find({
+    status: "Approved",
+  }).count();
+
+  return res
+    .status(200)
+    .json({ total_applications, being_reviewed, approved_applications });
 };
 
 export const approveApplicant = async (req, res) => {
